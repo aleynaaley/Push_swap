8111161d9c85089ef7949d9a69cf2949ee342ac2
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-06-30 02:38:35 +0300

    Update check_arg_3.c

--- push_swap/check_arg_3.c
+++ push_swap/check_arg_3.c
@@ -25,31 +25,27 @@ static int ft_isnum(char *num)
 	}
 	return(1);
 }
-int	ft_atoi(const char *str)
+long	ft_atoi(const char *str)
 {
-	int		i;
-	long	num;
+	long	i;
+	long	number;
 	int		sign;
 
 	i = 0;
-	num = 0;
+	number = 0;
 	sign = 1;
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+	while (str[i] && (str[i] == 32 || (str[i] >= 9 && str[i] <= 13)))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
-			sign *= -1;
+			sign = -1;
 		i++;
 	}
-	while (str[i] && ft_isdigit(str[i]))
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		num = (num * 10) + ((str[i] - '0') * sign);
+		number = (number * 10) + (str[i] - '0');
 		i++;
-		if (num > 2147483647)
-			return (-1);
-		if (num < -2147483648)
-			return (0);
 	}
-	return (num);
+	return (number * sign);
 }
